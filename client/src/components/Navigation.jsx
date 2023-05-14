054a6f166a877a328cb2b04d4793bbc37f24cecb
ManuRodriguezC <estebanclimb@gmail.com>
2023-05-13 19:51:40 -0500

    Clean Code

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export function Navigation() {
     return (
-        <div className="flex justify-around py-3">
+        <div className="flex justify-around py-8">
             <Link to="/tasks">
                 <h1 className="font-bold text-3xl mb-4">Task App</h1>
             </Link>
